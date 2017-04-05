97aafb8e531b7ca04c07d934b1a2d08fd39c831f
buddybuild <help@buddybuild.com>
2017-04-04 17:21:41 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- LoginTempl/AppDelegate.swift
+++ LoginTempl/AppDelegate.swift
@@ -28,6 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         
         //For Google Signin
